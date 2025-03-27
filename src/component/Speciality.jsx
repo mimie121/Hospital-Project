e3cc990262e5a2111cd6f10f211e62b728583d16
mimie121 <talk2mimie.reply@gmail.com>
2025-03-27 15:07:27 +0100

    Fixed scrolling and improved deployment

--- src/component/Speciality.jsx
+++ src/component/Speciality.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const Speciality = () => {
   return (
-    <div className='flex flex-col items-center gap-4 py-16 text-gray-800' id='speciality'>
+    <div className='flex flex-col items-center gap-4 py-16 text-gray-800' id="speciality">
 
       <h1 className='text-3xl font-medium'>Find by Speciality</h1>
       <p className='sm:w-1/3 text-center text-sm'>Browse through our extensive list of trusted doctors, schedule your appointment hassle-free.
