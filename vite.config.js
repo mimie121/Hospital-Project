7a323a1248cf0f00cd724604bd0eb3c5e0fd0985
mimie121 <talk2mimie.reply@gmail.com>
2025-03-27 10:38:09 +0100

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/hospital-project/",  
+  base: '/Hospital-Project/',
   server: {
     port: 3000,
     strictPort: true,
